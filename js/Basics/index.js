bbe8b9cfe9900da2bd0b97c94ff3731c83b4943d
Angel Zambrano <angelfguerraz@web.de>
2022-01-22 09:26:52 +0100

    changes in basic folder

--- js/Basics/index.js
+++ js/Basics/index.js
@@ -47,3 +47,73 @@ Boolean("a"); //true fro any char
 Boolean([]); //true for any array, despite empty
 Boolean({}); //true for any object
 Boolean(function () { }); //true for all functions
+
+// ARRAYS
+var a = []
+a.push(1, 2, 3) // pushes to the end
+a.pop(2) // pops last element 
+a.unshift(8) // shifts element to start
+a.shift(1)  // deletes first element 
+console.log(`a: ${a} type of a: ${typeof (a)} length = ${a.length} index of 1: ${a.indexOf(1)}`)
+
+// LOOPS
+var a = [0, 1, 2, 3, 4, 5]
+function display(n) {
+    console.log(`Index: ${a.indexOf(n)} Number: ${a[n]}`)
+}
+
+for (var i = 0; i < a.length; i++) {
+    display(i)
+}
+
+for (var n of a) {
+    display(n)
+}
+
+// OBJECTS
+class Person {
+    constructor(name, age, student) {
+        this.name = name,
+            this.age = age,
+            this.student = student
+    }
+}
+
+var p0 = new Person("Angel", 23, true)
+console.log(Person)
+console.log(typeof (Person))
+console.log(p0)
+console.log(typeof (p0))
+
+// Array Methods
+var array = [
+    { char: "a", ascii: "a".charCodeAt() },
+    { char: "b", ascii: "b".charCodeAt() },
+    { char: "c", ascii: "c".charCodeAt() },
+    { char: "d", ascii: "d".charCodeAt() },
+    { char: "e", ascii: "e".charCodeAt() },
+    { char: "@", ascii: "@".charCodeAt() },
+]
+
+// Filter return a new array
+var array_filter = array.filter((elem) => {
+    return elem.ascii <= 100
+});
+
+var array_map = array.map((elem) => {
+    return elem.char
+})
+
+var a_find = array.find((element)=>{
+    return element.char === "@"
+})
+
+//Some return a boolean
+var a_some = array.some((e)=>{
+    return e.ascii <=100
+})
+
+console.log(array_filter)
+console.log(array_map)
+console.log(a_find)
+console.log(a_some)
--- js/Basics/tempCodeRunnerFile.js
+++ js/Basics/tempCodeRunnerFile.js
@@ -1,10 +1,32 @@
-var person = {
-    name: "angel",
-    age: 34
-}
+// Array Methods
+var array = [
+    { char: "a", ascii: "a".charCodeAt() },
+    { char: "b", ascii: "b".charCodeAt() },
+    { char: "c", ascii: "c".charCodeAt() },
+    { char: "d", ascii: "d".charCodeAt() },
+    { char: "e", ascii: "e".charCodeAt() },
+    { char: "@", ascii: "@".charCodeAt() },
+]
 
-var greet = function (name) {
-    console.log(`Hello ${name}`)
-}
+// Filter return a new array
+var array_filter = array.filter((elem) => {
+    return elem.ascii <= 100
+});
 
-greet(person.name)
+var array_map = array.map((elem) => {
+    return elem.char
+})
+
+var a_find = array.find((element)=>{
+    return element.char === "@"
+})
+
+//Some return a boolean
+var a_some = array.some((e)=>{
+    return e.ascii <=100
+})
+
+console.log(array_filter)
+console.log(array_map)
+console.log(a_find)
+console.log(a_some)
